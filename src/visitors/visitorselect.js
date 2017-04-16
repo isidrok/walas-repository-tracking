8f8bdbf1defa719a42aa01659e3fed855f60702f
Isidro Torregrosa Torralba <isistorre95@gmail.com>
2017-04-16 15:47:06 +0200

    visitor select working fine

--- src/visitors/visitorselect.js
+++ src/visitors/visitorselect.js
@@ -1,15 +1,15 @@
 import { VisitorBase } from './visitorbase';
 export class VisitorSelect extends VisitorBase {
-    constructor(ast, entity, context, provider) {
-        super(ast, entity, context, provider);
+    constructor(expression, entity, context, provider) {
+        super(expression, entity, context, provider);
     }
-    ArrowFunctionExpression(node) {
-        node.body.parent = node;
+   ArrowFunctionExpression(node) {
+        let firstPrefix = this._provider.nextPrefix();
+        node.body.prefix = firstPrefix;
+        this._buildFrom(firstPrefix);
         this.visit(node.body);
     }
     ObjectExpression(node) {
-        node.prefix = this._provider.nextPrefix();
-        //if firstPrefix --> resolveFrom
         node.properties.forEach(c => {
             c.parent = node;
             c.prefix = node.prefix;
@@ -23,43 +23,42 @@ export class VisitorSelect extends VisitorBase {
             this.visit(c);
         });
     }
-    ObjectProperty(node) {
+    ObjectProperty(node) {  
+        node.value.prefix = node.prefix;  
         //let meta= getmeta() this.check(meta)
-        node.value.prefix = node.prefix;
-        if (node.value.type === 'ObjectExpression' || node.value.type === 'ArrayExpression') {
-            node.value.parent = node;
+        if (node.value.type !== 'Identifier'){
+            node.prefix = this._provider.nextPrefix();
             this._buildJoin(node);
+            node.value.parent = node;
+            node.value.prefix = node.prefix;
         }
-        //now we are inside a node who belongs to the main entity, thus its prefix
-        //is the one related to it and we can build the from statement.
-        else this._buildFrom(node);
-        this.visit(node.value);
+        this.visit(node.value);    
     }
-
     Identifier(node) {
         this._provider.grammar.select.push({ prefix: node.prefix, field: node.name });
     }
 
-    _buildFrom(node) {
-        //
-        if (this._provider.grammar.from.from) return;
-        let table = this._entity.name; //entity.meta.class.entity.table
+    _buildFrom(firstPrefix){
+        let table = this._entity.name; //this._entity.meta.class.entity.table
+        let provider = this._entity.name + 'provider'; //this._entity.meta.class.entity.provider
+        this._provider.grammar.from = {from: table, prefix: firstPrefix, provider: provider};
+    }
+    _buildJoin(node){
+        //{type:'left',table:'bar',prefix:'b', provider:'google',on:["id","id"],join:[]
         let prefix = node.prefix;
-        this._provider.grammar.from = { from: table, prefix: prefix };
-    }
-
-    _buildJoin(node) {
-        /*
-        If node.parent.prefix is in the array of joins and node.prefix is different
-        we go into the joins array of the parent node.
-
-        If node.parent.prefix is not in the array of joins then we are in the main join,
-        and we push the object in the grammar joins.
-        */
+        let table = node.key.name; //look in the metaentities for the name and extract table name from meta?
+        let required = true; //look in the meta of entity, then search for the property pointed by table and see if its required
+        let relation = 'hasOne'; //look in the meta of entity, then search for the property pointed by table and see the type or relation
+        let provider = 'google'; //look again for the meta of the property that creates the relaation with table and extract the provider
+        let on = ['id','id']; // some kind of convention??
+        let join = [];
+        let obj = {prefix:prefix, table:table, required:required, relation:relation, provider:provider, on:on, join:join};
         let joins = this._provider.grammar.join;
-        let isParent = joins.some(c => {
-            return c.prefix !== node.parent.prefix;
-        });
-        if (!isParent) this._provider.grammar.join.push({ prefix: node.prefix, join: [] });
-    }
+        let imInside = joins.filter(c=>c.prefix === node.prefix)[0];
+        if(!imInside){
+          let myParentIsInside = joins.filter(c=>c.prefix === node.parent.prefix)[0];
+          let destination = myParentIsInside || this._provider.grammar;
+          destination.join.push(obj);
+        } 
+    }     
 }
